2c97c810ddf56fb59f4398932f63781dd3ee61ec
minhnq410 <nqminh410@gmail.com>
2020-11-24 23:46:00 +0700

    Add overlapped-frame processing to remove noise due to end-effect

--- mdct.py
+++ mdct.py
@@ -1,5 +1,5 @@
 from tools import *
-from numpy import pad, array, zeros, dot
+from numpy import pad, array, zeros, dot, hstack
 import math
 
 N = 1000  # number of samples
@@ -10,7 +10,9 @@ def mdct_transform(sample_rate, data, file):
     print("Original data: ", data)
     print("Plotting original data...")
     plot_waveform(len(data) / sample_rate, data, "Original")
-    matrix = gen_matrix(N//2)
+    # matrix = gen_matrix(N//2)
+    # matrix to transform 2 frames at once
+    matrix = gen_matrix(N)
 
     print("Padding zeroes to original data...")
     numzeros = N - len(data) % N
@@ -25,20 +27,48 @@ def mdct_transform(sample_rate, data, file):
         count += 1
     # print(frame)
 
-    print("Performing MDCT on each frame...")
+    # Overlapping MDCT
+    print("Performing MDCT on signal...")
     mdct = []
-    for i in range(0, count):
-        tmp = dot(matrix, frame[i].T)
+    for i in range(0, count-1):
+        # Merge frame i with i+1 then perform MDCT
+        tmp = dot(matrix, hstack((frame[i],(frame[i+1]))).T)
         mdct.append(tmp)
-#    print(mdct)
+    #print(len(mdct))
 
+    # Inverse MDCT and split into halfs
     frame_inv = []
     for mdct_frame in mdct:
-        tmp = dot(matrix.T, mdct_frame)/N
-        frame_inv.append(tmp)
+        tmp = dot(matrix.T, mdct_frame)/(2*N)
+        frame_inv.append(tmp[:N])
+        frame_inv.append(tmp[N:])
+    #print(len(frame_inv))
+
+    # Resolve overlapping to reduce end-effect at the border of frames
+    reconstructed_frames = []
+    i = 0
+    while i < len(frame_inv): 
+        if (i == 0) or (i == len(frame_inv)-1):
+            # The first and the last frames are not overlapped
+            reconstructed_frames.append(frame_inv[i])
+            i+=1
+        else:
+            #print(i)
+            # Adding overlapped frames together
+            tmp = []
+            #print(len(frame_inv[i]))
+            #print(len(frame_inv[i+1]))
+            for j in range(0, len(frame_inv[i])):
+                #print(j)
+                tmp.append(frame_inv[i][j] + frame_inv[i+1][j])
+            reconstructed_frames.append(tmp)
+            i+=2
+    
+    #print(reconstructed_frames[0])
 
     reconstrusted_data = []
-    for imdct_frame in frame_inv:
+    # for imdct_frame in frame_inv:
+    for imdct_frame in reconstructed_frames:
         reconstrusted_data.extend(imdct_frame)
 
     reconstrusted_data = array(reconstrusted_data)[:len(data)].astype(data.dtype)
